dcdde6e40f813b1942a74fa99b82f4aa2a97a58d
Loc Tan Vo <loc.tan.vo@gmail.com>
2016-03-07 13:04:07 +0100

    Fixed collapse/expand all

--- collapseexpand.js
+++ collapseexpand.js
@@ -1,15 +1,10 @@
 var collapse = $('<span><a class="navlink">Collapse/Expand</a></span>');
 collapse.click(function(){
     var arrows = $('.arrow');
-    console.log(arrows);
     
     arrows.each(function(){
-        this.click();
-        //$(this).children().first().mousedown();
-        // $(this).attr("exp", "0");
-        // $(this).children().first().attr("class","flat-right-arrow-black");
-        //$(this).children().first().trigger("click");
-        console.log(this);
+        $(this).mouseover();
+        $(this).mousedown();
     });
 });
 
